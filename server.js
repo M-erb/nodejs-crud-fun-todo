2826d39339be999602f1f7282a5ef9574b6eda4e
Merb <michael.erbs@gmail.com>
2018-02-25 00:24:42 -0700

    added todo basic api

--- server.js
+++ server.js
@@ -1,55 +1,68 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const db = require('flat-db')
 
 app.use('/', express.static(__dirname + '/www'))
-
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html')
-// })
-
-
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json()
 
 db.configure({
   dir: './storage'
 })
 
-var ToDo = new db.Collection('movies', {
+var ToDo = new db.Collection('todo', {
   title: '',
   text: '',
-  priority: ''
+  priority: '',
+  isDone: false
 })
 
-// var keys = ToDo.add([{
-//   title: 'must do',
-//   text: 'This is an important todo',
-//   priority: 'high'
-// }, {
-//   title: 'must do',
-//   text: 'This is an important todo',
-//   priority: 'high'
-// }, {
-//   title: 'must do',
-//   text: 'This is an important todo',
-//   priority: 'high'
-// }, {
-//   title: 'must do',
-//   text: 'This is an important todo',
-//   priority: 'high'
-// }, {
-//   title: 'must do',
-//   text: 'This is an important todo',
-//   priority: 'high'
-// }])
-
-// console.log('\nkeys returned after adding multi items:');
-// console.log(keys);
-
-app.get('/todos', (req, res) => {
+app.get('/get-todos', (req, res) => {
   res.send(ToDo.all())
 })
-app.get('/todos-count', (req, res) => {
-  res.send(JSON.stringify(ToDo.count()))
+app.put('/update-todo', jsonParser, (req, res) => {
+  if (req.body.id && req.body.data) {
+    var key = ToDo.update(req.body.id, req.body.data)
+    res.status(200).send({
+      result: key
+    })
+  } else {
+    res.status(400).send({
+      result: "Required parameters missing"
+    })
+  }
+})
+app.post('/add-todo', jsonParser, (req, res) => {
+  if (req.body) {
+    var key = ToDo.add(req.body)
+    res.status(200).send({
+      result: key
+    })
+  } else {
+    res.status(400).send({
+      result: "Required parameters missing"
+    })
+  }
+})
+app.delete('/delete-todo', jsonParser, (req, res) => {
+  if (req.body.id) {
+    var payload = ToDo.remove(req.body.id)
+    res.status(200).send({
+      result: payload
+    })
+  } else {
+    res.status(400).send({
+      result: "Required parameters missing"
+    })
+  }
+})
+
+
+app.get('/count-todos', (req, res) => {
+  res.status(200).send({
+    result: ToDo.count()
+  })
 })
 
 app.listen(3000);
