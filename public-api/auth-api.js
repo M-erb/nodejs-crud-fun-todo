9fa6a7fd4423ed42fe7a6ab7fa78a29237f2c9b6
Merb <michael.erbs@gmail.com>
2018-03-10 12:21:22 -0700

    added auth end points

--- public-api/auth-api.js
+++ public-api/auth-api.js
@@ -5,7 +5,6 @@ const User = require('../models/user')
 
 module.exports = (app) => {
   app.post('/api/regestration', config.jsonParser, (req, res) => {
-    console.log('regestration', req.body)
     if (req.body) {
       // required fields
       if (!req.body.userName) return res.status(400).send({
@@ -30,6 +29,9 @@ module.exports = (app) => {
             lastName: req.body.lastName || '',
             emailAddress: req.body.emailAddress || ''
           })
+          res.status(200).send({
+            result: User.all()
+          })
         })
       })
     } else {
@@ -37,5 +39,55 @@ module.exports = (app) => {
         result: 'Required fields missing'
       })
     }
+  })
+
+  app.post('/api/login', config.jsonParser, (req, res) => {
+    console.log('api/login', req.body)
+    if (req.body) {
+      // required fields
+      if (!req.body.userName) return res.status(400).send({
+        result: 'User Name is required'
+      })
+      if (!req.body.password) return res.status(400).send({
+        result: 'Password is required'
+      })
+
+      let users = User.all()
+      console.log('users', users, users.length)
+      if (users.length) {
+        console.log('in if statement, right before for loop');
+        for (let i; i < users.length; i++) {
+          console.log('in loop', users[i], i);
+          // if (users[i].userName === req.body.userName) {
+          //   bcrypt.compare(req.body.password, users[i].password, (bErr, bRes) => {
+          //     if (res) {
+          //       res.status(200).send({
+          //         result: "logged in!"
+          //       })
+          //     } else {
+          //       result: err
+          //     }
+          //   })
+          //   break
+          // } else if (i > users.length) {
+          //   res.status(400).send({
+          //     result: 'User name does not exist'
+          //   })
+          //   break
+          // }
+        }
+      } else {
+        res.status(400).send({
+          result: 'User name and/or password is incorrect'
+        })
+      }
+    } else {
+      res.status(400).send({
+        result: 'Required fields missing'
+      })
+    }
+    // res.status(500).send({
+    //   result: 'Unhandled exeption'
+    // })
   })
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.use('/', express.static(__dirname + '/www'))
 
 require('./secure-api/todo-api')(app)
-
+require("./public-api/auth-api")(app)
 app.listen(3000);
 console.log('Server Started, listening on port 3000')
 console.log('visit: http://localhost:3000')
